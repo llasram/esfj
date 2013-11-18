da0b448b7ab38e5f55919689ade2ae750e55ba6d
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-11-17 19:41:42 -0500

    Begin work on next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.platypope/esfj "0.2.1"
+(defproject org.platypope/esfj "0.2.2-SNAPSHOT"
   :description "A Clojure library for defining JSR-330 Provider classes."
   :url "http://github.com/llasram/esfj"
   :license {:name "Eclipse Public License"
